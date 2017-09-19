d53b2946937232bbb356b734ff69ae6db362fea8
Ivan Pico Martin <ivan.pico.martin.sa@everis.com>
2017-09-19 11:58:30 +0200

    First Overlay version access contacts

--- app/src/main/java/com/alloverly/example/MainActivity.java
+++ app/src/main/java/com/alloverly/example/MainActivity.java
@@ -1,10 +1,26 @@
 package com.alloverly.example;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CONTACT = 123;
+    private static final int PICK_CONTACT = 124;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +28,106 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent svc = new Intent(this, OverlayShowingService.class);
         startService(svc);
-        finish();
+//        finish();
+
+
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askForContactPermission();
+            }
+        });
+
     }
+
+    private void getContact(){
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+
+    public void askForContactPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+
+
+                    requestPermissions(
+                            new String[]
+                                    {Manifest.permission.READ_CONTACTS}
+                            , PERMISSION_REQUEST_CONTACT);
+
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                    builder.setTitle("Contacts access needed");
+//                    builder.setPositiveButton(android.R.string.ok, null);
+//                    builder.setMessage("please confirm Contacts access");
+//                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+//                        @TargetApi(Build.VERSION_CODES.M)
+//                        @Override
+//                        public void onDismiss(DialogInterface dialog) {
+//                            requestPermissions(
+//                                    new String[]
+//                                            {Manifest.permission.READ_CONTACTS}
+//                                    , PERMISSION_REQUEST_CONTACT);
+//                        }
+//                    });
+//                    AlertDialog alertDialog = builder.show();
+//                    Log.d("TAGTAG","Size x " + alertDialog.getWindow().getAttributes().x);
+//                    Log.d("TAGTAG","Size y " + alertDialog.getWindow().getAttributes().y);
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            PERMISSION_REQUEST_CONTACT);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else{
+                getContact();
+            }
+        }
+        else{
+            getContact();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CONTACT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContact();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Log.d("TAGTAG","No permission for contacts");
+//                    Toast.makeText(getApplicationContext(),"No permission for contacts",Toast.LENGTH_LONG).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 }
--- app/src/main/java/com/alloverly/example/OverlayShowingService.java
+++ app/src/main/java/com/alloverly/example/OverlayShowingService.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,12 +19,10 @@ import android.widget.Toast;
 /**
  * Created by ipicomar on 18/09/2017.
  * ref: https://gist.github.com/bjoernQ/6975256
- *
- *  WORKS ON API 23 (DOESN'T WORK ON 25)
- *
+ * <p>
  */
 
-public class OverlayShowingService extends Service implements View.OnTouchListener, View.OnClickListener {
+public class OverlayShowingService extends Service implements View.OnClickListener {
 
     private View topLeftView;
 
@@ -32,6 +33,8 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
     private int originalYPos;
     private boolean moving;
     private WindowManager wm;
+    private WindowManager.LayoutParams params;
+    private Handler handler;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -42,48 +45,87 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
     public void onCreate() {
         super.onCreate();
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // acciones que se ejecutan tras los milisegundos
+                wm.addView(overlayedButton, params);
+            }
+        }, 1000);
+
         wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
 
+        Point start = new Point();
+        Point end = new Point();
+
+        overlyPermission(start);
+
         overlayedButton = new Button(this);
-        overlayedButton.setText("Overlay button");
-        overlayedButton.setOnTouchListener(this);
-        overlayedButton.setAlpha(0.0f);
-        overlayedButton.setBackgroundColor(Color.BLACK);
+        overlayedButton.setText("Allow this application to access Internet?");
+//        overlayedButton.setOnTouchListener(this);
+        overlayedButton.setAlpha(0.9f);
+        overlayedButton.setTextColor(Color.DKGRAY);
+        overlayedButton.setBackgroundColor(Color.WHITE);
 //        overlayedButton.setBackgroundColor(0x55fe4444);
         overlayedButton.setOnClickListener(this);
+        overlayedButton.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+        overlayedButton.setPadding(40,0,0,0);
 
-        WindowManager.LayoutParams params = new WindowManager.
-                LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+        params = new WindowManager.
+                LayoutParams(
+                        WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-//                WindowManager.LayoutParams.TYPE_TOAST,
+//                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_TOAST,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                         WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 PixelFormat.TRANSLUCENT);
 
-        params.gravity = Gravity.LEFT | Gravity.TOP;
-        params.x = 0;
-        params.y = 0;
-        wm.addView(overlayedButton, params);
+//        params.gravity = Gravity.CENTER;// | Gravity.START;
+//        params.gravity = Gravity.LEFT | Gravity.START;
+//        params.x = pxToDp(start.x);
+        params.y = start.y;
+        params.height = 240;
+        params.width = 800;
+//        params.height = pxToDp(850);
+//        params.x = pxToDp(start.x);
+//        params.y = pxToDp(start.y);
 
-        topLeftView = new View(this);
-        WindowManager.LayoutParams topLeftParams = new WindowManager.
-                LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+//        topLeftView = new View(this);
+//        WindowManager.LayoutParams topLeftParams = new WindowManager.
+//                LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+//                WindowManager.LayoutParams.WRAP_CONTENT,
 //                WindowManager.LayoutParams.TYPE_TOAST,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-                PixelFormat.TRANSLUCENT);
-
-        topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
-        topLeftParams.x = 0;
-        topLeftParams.y = 0;
-        topLeftParams.width = 0;
-        topLeftParams.height = 0;
-        wm.addView(topLeftView, topLeftParams);
+////                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+//                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+//                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+//                PixelFormat.TRANSLUCENT);
+//
+//        topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
+//        topLeftParams.x = 0;
+//        topLeftParams.y = 0;
+//        topLeftParams.width = 0;
+//        topLeftParams.height = 0;
+//        wm.addView(topLeftView, topLeftParams);
 
     }
+
+
+    private void overlyPermission(Point start) {
+        int heightDialog = dpToPx(740);
+        int widthDialog = dpToPx(720);
+        Point screenSize = new Point();
+        Point point = new Point();
+
+        WindowManager winMng = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+        winMng.getDefaultDisplay().getRealSize(screenSize);
+
+        start.y = (screenSize.y / 2) - (heightDialog / 2);
+        start.x = (screenSize.x / 2) - (widthDialog / 2);
+
+    }
+
 
     @Override
     public void onDestroy() {
@@ -96,60 +138,19 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
         }
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            float x = event.getRawX();
-            float y = event.getRawY();
-
-            moving = false;
-
-            int[] location = new int[2];
-            overlayedButton.getLocationOnScreen(location);
-
-            originalXPos = location[0];
-            originalYPos = location[1];
-
-            offsetX = originalXPos - x;
-            offsetY = originalYPos - y;
-
-        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            int[] topLeftLocationOnScreen = new int[2];
-            topLeftView.getLocationOnScreen(topLeftLocationOnScreen);
-
-            System.out.println("topLeftY="+topLeftLocationOnScreen[1]);
-            System.out.println("originalY="+originalYPos);
-
-            float x = event.getRawX();
-            float y = event.getRawY();
-
-            WindowManager.LayoutParams params = (WindowManager.LayoutParams) overlayedButton.getLayoutParams();
-
-            int newX = (int) (offsetX + x);
-            int newY = (int) (offsetY + y);
-
-            if (Math.abs(newX - originalXPos) < 1 && Math.abs(newY - originalYPos) < 1 && !moving) {
-                return false;
-            }
-
-            params.x = newX - (topLeftLocationOnScreen[0]);
-            params.y = newY - (topLeftLocationOnScreen[1]);
-
-            wm.updateViewLayout(overlayedButton, params);
-            moving = true;
-        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-            if (moving) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     @Override
     public void onClick(View v) {
         Toast.makeText(this, "Overlay button click event", Toast.LENGTH_SHORT).show();
     }
 
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public int pxToDp(int px) {
+        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
 }
